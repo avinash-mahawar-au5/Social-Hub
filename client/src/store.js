fdb5a4a14cd10fb89832b667fd55a2035f15d98f
avinash-mahawar-au5 <avi.mecrc@gmail.com>
2020-06-22 20:59:05 +0530

    Modified Profile AppRouter Store

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,21 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import appReducer from './reducers/app';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import appReducer from "./reducers/app";
+import profileReducer from "./reducers/profile";
+import usersReducer from "./reducers/users";
+import postsReducer from "./reducers/posts";
+import thunk from "redux-thunk";
 
 const reducers = combineReducers({
-	app: appReducer,
-	// profile: profileReducer,
-	// posts: postsReducer,
-	// users: usersReducer
+  app: appReducer,
+  profile: profileReducer,
+  posts: postsReducer,
+  users: usersReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
