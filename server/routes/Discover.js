c915e3b1eb92894ccb89e34bf026743cf4ae141e
avinash-mahawar-au5 <avi.mecrc@gmail.com>
2020-06-23 00:42:43 +0530

    Adding Server db model routes

---
+++ server/routes/Discover.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const { count } = require("../models/User");
+
+router.get("/users", (req, res) => {
+  User.countDocuments().exec((err, count) => {
+    if (err)
+      return res.status(500).send("There were an error counting the users");
+
+    const random = Math.floor(Math.random() * (count - 10));
+    User.find({})
+      .limit(10)
+      .skip(random)
+      .exec((err, result) => {
+        if (err)
+          return res.status(500).json({
+            message: "There were an error fetching the Users",
+            error: err,
+          });
+
+        res.status(200).json({
+          code: 200,
+          response: [...result],
+        });
+      });
+  });
+});
+
+router.get("/posts", (req, res) => {
+  Post.countDocuments().exec((err, count) => {
+    if (err) return res.status(500).send("post Couldn't be counted");
+
+    const random = MATH.floor(Math.random() * (count - 10));
+    Post.find({})
+      .limit(10)
+      .skip(random)
+      .populate("author")
+      .exec((err, result) => {
+        if (err)
+          return res.status(500).json({
+            message: "Post coudn't be fetched",
+            error: err,
+          });
+
+        res.status(200).json({
+          code: 200,
+
+          response: [...result],
+        });
+      });
+  });
+});
+
+module.exports = router;
